1536662e08b6beee847b3beeea0339ad1ae6b67f
Lucas Emanuel <131834616+llucasEmanuel@users.noreply.github.com>
2025-03-27 20:12:45 -0300

    feat(lcd): LCD funcional

--- Software/include/liquid_crystal/liquid_crystal.h
+++ Software/include/liquid_crystal/liquid_crystal.h
@@ -1,12 +1,19 @@
 #ifndef __LIQUID_CRYSTAL_H__
 #define __LIQUID_CRYSTAL_H__
 
-#define WRITE = 1;
-#define WAIT = 0;
-extern char state;
-
-void lcd_write(char *);
+void lcd_write_msg(char *);
+void lcd_write_char(char);
 void lcd_write_info(int, unsigned int);
-int lcd_init(int);
+void lcd_init(int);
 
 #endif /* __LIQUID_CRYSTAL_H__ */
+
+/* 
+pulseEnable
+init
+clear
+home
+setCursor
+write
+noCursor
+*/
