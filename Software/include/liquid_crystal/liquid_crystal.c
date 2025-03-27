a6831654da81b07be2a75eadf109f9a6dfed6a2b
Lucas Emanuel <131834616+llucasEmanuel@users.noreply.github.com>
2025-03-27 20:12:04 -0300

    feat(lcd): LCD funcional

--- Software/include/liquid_crystal/liquid_crystal.c
+++ Software/include/liquid_crystal/liquid_crystal.c
@@ -6,13 +6,23 @@
 
 char state = WRITE;
 
+void lcd_write_msg(char *msg) {
+	
+}
+
+void lcd_write_char(char c) {
+	
+}
+
 
 void lcd_write_info(int fd, unsigned int lcd_info) {
 
     ioctl(fd, WR_LCD);
-    write(fd, &lcd_info, sizeof(lcd_info));    
-    lcd_info = lcd_info & 0b11110111111111;
-    write(fd, &lcd_info, sizeof(lcd_info));  
+    write(fd, &lcd_info, 4);  
+    for(int i = 0; i < 30000; i++) {
+    }  
+    lcd_info = lcd_info & 0b11111111111111111111110111111111;
+    write(fd, &lcd_info, 4);  
 
 }
 
@@ -20,28 +30,28 @@ void lcd_init(int fd) {
 
     unsigned int lcd_info = 0;
 
-    lcd_info = 0x38 | (1 << 9) | (0 << 13);
+    lcd_info = 0b0000000000000000001000001000111000;
 
     lcd_write_info(fd, lcd_info);
 
     
-    lcd_info = 0x0e | (1 << 9) | (0 << 13);
+    lcd_info = 0b0000000000000000001000001000001110;
     lcd_write_info(fd, lcd_info);
 
     
-    lcd_info = 0x01 | (1 << 9) | (0 << 13);
+    lcd_info = 0b0000000000000000001000001000000001;
     lcd_write_info(fd, lcd_info);
 
     
-    lcd_info = 0x02 | (1 << 9) | (0 << 13);
+    lcd_info = 0b0000000000000000001000001000000010;
     lcd_write_info(fd, lcd_info);
 
     
-    lcd_info = 0x06 | (1 << 9) | (0 << 13);
+    lcd_info = 0b0000000000000000001000001000000110;
     lcd_write_info(fd, lcd_info);
 
     // Escreve H para teste    
-    lcd_info = 0x48 | (1 << 9) | (1 << 13);
+    lcd_info = 0b0000000000000000001010001001001000;
     lcd_write_info(fd, lcd_info);
 
 }
