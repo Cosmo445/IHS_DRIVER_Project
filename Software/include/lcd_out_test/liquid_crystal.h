8354c6397cc9563ea4b4ecef6f3cc41cc619ad09
Gabriel Alves <136081929+Gagdm@users.noreply.github.com>
2025-03-29 16:37:26 -0300

    feat(lcd): integrations tested and functionals

---
+++ Software/include/lcd_out_test/liquid_crystal.h
@@ -0,0 +1,26 @@
+#ifndef __LIQUID_CRYSTAL_H__
+#define __LIQUID_CRYSTAL_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+void lcd_write_msg(int, char *);
+void lcd_write_char(int, char);
+void lcd_write_control(int, char);
+void lcd_drive_write(int, uint32_t);
+void lcd_init(int);
+void lcd_clear(int);
+
+/* 
+home
+setCursor
+noCursor
+*/
+
+extern int update_data(char, char);
+extern void pulse_enable(int, uint32_t);
+
+#endif /* __LIQUID_CRYSTAL_H__ */
+
+
---
+++ Software/include/lcd_out_test/main.c
@@ -0,0 +1,8 @@
+#include "liquid_crystal.h"
+
+int main() {
+
+    lcd_write_msg(1, "abcdefghijkl");
+    return 0;
+
+}
