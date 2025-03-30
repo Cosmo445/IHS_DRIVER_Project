df54a4db729ffd5ae4215b187592361f8170151f
rama39 <rama@cin.ufpe.br>
2025-03-30 04:57:58 +0000

    feat(game): preparado pra inserção das linhas

--- Software/src/main.c
+++ Software/src/main.c
@@ -1,5 +1,7 @@
 #define RODANDO_NA_PLACA 0
 
+#define DEV_MODO 1
+
 #if RODANDO_NA_PLACA
 
 #include <stdio.h>	/* printf */
@@ -24,7 +26,7 @@
 //https://github.com/raysan5/raylib/discussions/2478
 
 
-// Funcoes hardware (((TODO: fazer)))
+// Funcoes hardware
 void readBotoes();
 char botao____(char);
 char botaoLeft();
@@ -36,8 +38,6 @@ void readSwitches();
 int switches();
 int switch_(int);
 
-void leds_verm(int);
-void leds_verd(int);
 char contaBits[16];
 void initContaBit();
 char forcaReator();
@@ -46,12 +46,17 @@ char energiaArmas();
 char quantasVelas();
 char quantasValvulas();
 
+int mapeiaDir(int);
+int mapeiaEsq(int, int);
+void writePerifericos();
+void resetPerifericos();
+
 char rd_botoes;
-#ifdef O_RDWR
+#if RODANDO_NA_PLACA
 int fd;
 #endif
 void readBotoes() {
-    #ifdef RD_PBUTTONS
+    #if RODANDO_NA_PLACA
     // LÊ DOS BOTOES DA PLACA
     ioctl(fd, RD_PBUTTONS);
     read(fd, &rd_botoes, 1);
@@ -86,9 +91,10 @@ char botaoRight() {
 
 int rd_switches;
 void readSwitches() {
-    #ifdef RD_SWITCHES
+    #if RODANDO_NA_PLACA
+    // LÊ DOS SWICHES
     ioctl(fd, RD_SWITCHES);
-    read(fd, &rd_switches, 4);
+    read(fd, &rd_switches, sizeof(rd_switches));
     #else
     static int pressed_prev = 0, pressed_now = 0, pressed_true;
     pressed_now =
@@ -123,13 +129,6 @@ int switch_(int qual){
     return rd_switches&(1<<qual);
 }
 
-void leds_verm(int valor) {
-    ;
-}
-void leds_verd(int valor) {
-    ;
-}
-
 void initContaBit() {
     // TODO implementar em assembly
     for(int i = 0; i < 16; i++) {
@@ -154,6 +153,83 @@ char quantasValvulas() {
     return contaBits[(switches()>> 0)&15];
 }
 
+const int map_7_seg[10] = { // 1 is active
+    0b00111111, // 0
+    0b00000110, // 1
+    0b01011011, // 2
+    0b01001111, // 3
+    0b01100110, // 4
+    0b01101101, // 5
+    0b01111101, // 6
+    0b00000111, // 7
+    0b01111111, // 8
+    0b01101111  // 9
+};
+int mapeiaDir(int bat) {
+    return ~(
+        (map_7_seg[bat/1000%10]<<24) +
+        (map_7_seg[bat/100 %10]<<16) +
+        (map_7_seg[bat/10  %10]<< 8) +
+        (map_7_seg[bat     %10]    )
+    );
+}
+int mapeiaEsq(int tem, int pre) {
+    return ~(
+        (map_7_seg[tem/10%10]<<24) +
+        (map_7_seg[tem   %10]<<16) +
+        (map_7_seg[pre/10%10]<<8) +
+        (map_7_seg[pre   %10])
+    );
+}  
+void writePerifericos() {
+    #if RODANDO_NA_PLACA
+    
+    static int mapped;
+    // ESCREVE BATERIA NO 7SEG DA DIREITA
+    mapped = mapeiaDir(BATERIA);
+    ioctl(fd, WR_R_DISPLAY);
+    write(fd, &mapped, sizeof(mapped));
+    // ESCREVE TEMP e PRESSAO NOS 7SEG DA ESQUERDA
+    mapped = mapeiaEsq(TEMPERATURA, PRESSAO);
+    ioctl(fd, WR_L_DISPLAY);
+    write(fd, &mapped, sizeof(mapped));
+    
+    // ESCREVE LEITURA NOS LEDS
+    ioctl(fd, WR_RED_LEDS);
+    write(fd, &rd_switches, sizeof(rd_switches));
+    // ANIMACAO DOS LEDS VERDES
+    int verde = rand();
+    ioctl(fd, WR_GREEN_LEDS);
+    write(fd, &verde, sizeof(verde));
+    
+    #else 
+    // fazer nada
+    #endif
+}
+void resetPerifericos() {
+    #if RODANDO_NA_PLACA
+    
+    static int reset;
+    
+    // RESETA LEDS
+    reset = 0;
+    ioctl(fd, WR_RED_LEDS);
+    write(fd, &reset, sizeof(reset));
+    ioctl(fd, WR_GREEN_LEDS);
+    write(fd, &reset, sizeof(reset));
+    
+    // RESETA 7SEG DA DIREITA E DA ESQUERDA
+    reset = ~0;
+    ioctl(fd, WR_R_DISPLAY);
+    write(fd, &reset, sizeof(reset));
+    ioctl(fd, WR_L_DISPLAY);
+    write(fd, &reset, sizeof(reset));
+    
+    #else 
+    // fazer nada
+    #endif
+}
+
 
 // Cenas do jogo
 enum cenas_enum {
@@ -167,7 +243,6 @@ int cena_atual = MENU;
 
 int FASE = 1;
 
-
 // Update de variáveis
 void runMenu();
 void runControles();
@@ -180,7 +255,10 @@ enum opcoes_menu {
     op_JOGO = 2
 };
 int opcao = 0;
-    
+int BATERIA = 1000, PRESSAO = 0, TEMPERATURA = 0;
+int COMBUSTIVEL = 10000, FLUIDO = 100;
+int DANOS = 0;
+
 void runMenu() {
     if(botaoRight()) {
         if(opcao==2) opcao = 0;
@@ -191,7 +269,9 @@ void runMenu() {
         else opcao--;
     }
     
-    if(botaoUp()) cena_atual = opcao+1;
+    if(botaoUp())
+    	cena_atual = opcao+1,
+        BATERIA = 1000, PRESSAO = 0, TEMPERATURA = 0;
 }
 void runControles() {
     if(botaoDown()) cena_atual = MENU;
@@ -202,7 +282,10 @@ void runCreditos() {
 void runJogo() {
     
     // slides
-    if(botaoDown()) cena_atual = MENU;
+    if(botaoDown()) {
+        cena_atual = MENU, resetPerifericos();
+        return;
+    }
     if(botaoRight()) {
         if(FASE<3) FASE++;
         else FASE = 1;
@@ -214,6 +297,7 @@ void runJogo() {
     
     // Coisas que rodam em todas a fases
     readSwitches();
+    writePerifericos();
     
     // Coisas que so rodam em algumas fases
     switch (FASE) {
@@ -221,9 +305,13 @@ void runJogo() {
             //
         break;
         case 2:
+    		BATERIA--;
             //
         break;
         case 3:
+    		BATERIA--;
+        	PRESSAO++;
+        	TEMPERATURA++;
             //
     }
 }
@@ -248,6 +336,13 @@ Texture2D
 
 int ScWi, ScHe;
 int _x1, _x2, _y1, _y2; //coordenadas para print no menu
+// TODO: make static or global (to precompute)
+int _SwWi, _SwHe, _GapWi;
+
+int ponto_qtd[18] = {
+    4,3,3,4, 4,3,3,4, 4,3,3,4, 10,10, 4,3,3,4
+};
+Vector2 pontos[18][10] = {};
 
 // TODO: fundos menu, controles, creditos
 void initFundos() {
@@ -313,6 +408,13 @@ void telaCreditos() {
     
     DrawText("CREDITOS", 190, 200, 20, GRAY);
 }
+
+void DrawLineStripEx(const Vector2 *points, int pointCount, float thick, Color color) {
+    static int i;
+    for(i=1;i<pointCount;i++) 
+        DrawLineEx(points[i-1], points[i], thick, color);
+}
+
 void telaJogo() {
     ClearBackground(RAYWHITE);
     
@@ -333,53 +435,61 @@ void telaJogo() {
     
     // Elementos visuais de todas a fases
     
-    // TODO: make static or global (to precompute)
-    int SwWi = ScWi/40, SwHe=ScHe/15, GapWi = (ScWi-SwWi*18)/19;
-    for(int i = 0; i < 18; i++) {
-        
+    #define DrawSwitchRec() \
+    DrawRectangle( \
+        _GapWi + (i)*(_GapWi+_SwWi), \
+        ScHe - (ScHe/10), \
+        _SwWi, _SwHe, \
+        _cor \
+    )
+    #define DrawNetLines() \
+    DrawLineStripEx( pontos[i], ponto_qtd[i], 3, _cor )
+    static Color _cor;
+    Color CORON = (Color){200, 10, 25,255}, COROFF = (Color){ 40, 40, 25,255};
+    int i = 0;
+    for(; i < 12; i++) {
+        _cor = ((FASE!=1) && switch_(17-i)) ?  CORON : COROFF;
         // TODO: desenha 'net' do botao, fios que conectam botao a um componente
         // cor da net muda quando se aperta um botao; tipo redstone
         
-        DrawRectangle(
-            GapWi+(i)*(GapWi+SwWi), 
-            ScHe-(ScHe/10), 
-            SwWi, SwHe, 
-            switch_(17-i) ? (Color){200,10,10,255} : (Color){10,10,10,255}
-        );
+        DrawNetLines();
+        DrawSwitchRec();
+    }
+    for(; i < 18; i++) {
+        _cor = ((FASE==3) && switch_(17-i)) ?  CORON : COROFF;
+        
+        DrawNetLines();
+        DrawSwitchRec();
     }
 }
 
 
 // Cena separada de início do jogo
 int frame = 0;
-int fade_frames = 24;
+const int fade_frames = 24;
 void animarfade(Color *fade) {
-    if(frame<=fade_frames) {
-        
-        fade->a -= 255/fade_frames;
-        if(frame==fade_frames) fade->a = 0;
-        
-        DrawRectangle(0, 0, ScWi, ScHe, *fade);
-        
-        frame++;
-    }
+    fade->a -= 255/fade_frames;
+    if(frame==fade_frames) fade->a = 0;
+    
+    DrawRectangle(0, 0, ScWi, ScHe, *fade);
 }
 void animartexto(char *texto) {
     static int ind = 0;
     static char temp;
     
-    if(frame>fade_frames) {
-        if(texto[ind]) ind++;
-        
-        temp = texto[ind];
-        texto[ind] = 0;
-        
-        DrawText(texto, 190, 200, 20, GRAY);
-        
-        texto[ind] = temp;
-        
-        frame++;
-    }
+    if(texto[ind]) ind++;
+    if(botaoRight())
+        for(int i = 0; i < 10 && texto[ind]; i++)
+            ind++; 
+    
+    temp = texto[ind];
+    texto[ind] = 0;
+    
+    DrawText(texto, 190, 200, 20, GRAY);
+    
+    texto[ind] = temp;
+    
+    frame++;
 }
 void jogoInicio() {
     
@@ -392,22 +502,108 @@ porta turpis. Morbi scelerisque sem vel risus ultrices, ut iaculis odio posuere.
 ";
     
     SetTargetFPS(24);
-    while(!WindowShouldClose() && !botaoUp()) {
-        
+    while(!WindowShouldClose() && !botaoUp() && frame <= fade_frames) {
         readBotoes();
-        
         _BD
             ClearBackground(RAYWHITE);
-            
             animarfade(&fade);
-            
-            animartexto(texto);
-            
         _ED
+        frame++;
+    }
+    while(!WindowShouldClose() && !botaoUp()) {
+        readBotoes();
+        _BD
+            ClearBackground(RAYWHITE);
+            animartexto(texto);
+        _ED
+        frame++;
     }
     SetTargetFPS(12);
 }
 
+void dev_print() {
+    char texto[1024];
+    snprintf(texto, 1024, 
+"COMBUSTIVEL: %i\n\
+BATERIA: %i\n\
+PRESSAO: %i\n\
+TEMPERATURA: %i\n\
+FLUIDO: %i\n\
+LCD:\n\
+|\n\
+|\n", 
+    COMBUSTIVEL, BATERIA, PRESSAO, TEMPERATURA, FLUIDO);
+    DrawText(texto, 10, 10, 20, BLACK);
+}
+
+void initGeral() {
+    
+    InitWindow(0, 0, "jogo nave");
+    SetTargetFPS(12);
+    
+    ToggleFullscreen();
+    HideCursor();
+    
+    int temp = GetCurrentMonitor();
+    ScWi = GetMonitorWidth(temp);
+    ScHe = GetMonitorHeight(temp);
+    _x1 = (int)(0.26  * ((double)ScWi));
+    _x2 = (int)(0.47  * ((double)ScWi));
+    _y1 = (int)(0.40  * ((double)ScHe));
+    _y2 = (int)(0.475 * ((double)ScHe));
+    _SwWi = ScWi/40, _SwHe = ScHe/15; 
+    _GapWi = (ScWi-_SwWi*18)/19;
+    
+    // TODO: estabelecer pontos de referencia (em funcao de ScWi ou ScHe)
+    int Y0 = 0, YFa = 0, YFb = 0;
+    Vector2 pontos_ref[18][10] = { 
+ 
+    {{}, {0, Y0}, {0, Y0 }, {0, YFa}},
+    {{}, {0, Y0}, {0, YFa}},
+    {{}, {0, Y0}, {0, YFa}},
+    {{}, {0, Y0}, {0, Y0 }, {0, YFa}},
+    
+    {{}, {0, Y0}, {0, Y0 }, {0, YFa}},
+    {{}, {0, Y0}, {0, YFa}},
+    {{}, {0, Y0}, {0, YFa}},
+    {{}, {0, Y0}, {0, Y0 }, {0, YFa}},
+    
+    {{}, {0, Y0}, {0, Y0 }, {0, YFa}},
+    {{}, {0, Y0}, {0, YFa}},
+    {{}, {0, Y0}, {0, YFa}},
+    {{}, {0, Y0}, {0, Y0 }, {0, YFa}},
+    
+    {{}, {0, Y0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, YFb}},
+    {{}, {0, Y0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, YFb}},
+    
+    {{}, {0, Y0}, {0, Y0 }, {0, YFa}},
+    {{}, {0, Y0}, {0, YFa}},
+    {{}, {0, Y0}, {0, YFa}},
+    {{}, {0, Y0}, {0, Y0 }, {0, YFa}}
+    
+    };
+    
+    // pontos base
+    for(int i = 0; i < 18; i++)
+        pontos[i][0] = (Vector2){_GapWi + (i)*(_GapWi+_SwWi) + _SwWi/2, ScHe - (ScHe/10)};
+    // pontos da referencia
+    for(int i = 0; i < 18; i++)
+        for(int j = 1; j < 10; j++)
+            pontos[i][j] = pontos_ref[i][j];
+    // possiveis pontos computaveis
+    for(int i = 0; i < 18; i++)
+        pontos[i][3] = (Vector2){pontos[i][0].x, ScHe - (ScHe/7)};
+    
+    initFundos();
+    
+    initContaBit();
+    
+    #if RODANDO_NA_PLACA
+    fd = open(argv[1], O_RDWR);
+    #endif
+    
+    /* TODO: cria THREAD para leitura de switches, botões e escrita de leds.. */
+}
 
 //variavel global lida por cada thread para verificar se o jogo foi fechado
 int jogo_fechado = 0; 
@@ -419,34 +615,10 @@ int main(int argc, char** argv)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-
-    InitWindow(0, 0, "jogo nave");
-    SetTargetFPS(12);
-    
-    ToggleFullscreen();
-    HideCursor();
-    
-    int temp = GetCurrentMonitor();
-    ScWi = GetMonitorWidth(temp);
-    ScHe = GetMonitorHeight(temp);
-        _x1 = (int)(0.26  * ((double)ScWi)),
-        _x2 = (int)(0.47  * ((double)ScWi)),
-        _y1 = (int)(0.40  * ((double)ScHe)),
-        _y2 = (int)(0.475 * ((double)ScHe))
-    ;
-    
-    #ifdef O_RDWR
-    fd = open(argv[1], O_RDWR);
-    #endif
-    
-    initFundos();
-    
-    initContaBit();
-    
-    /* TODO: cria THREAD para leitura de switches, botões e escrita de leds.. */
-    
+    initGeral();
     //--------------------------------------------------------------------------------------
     
+    // Cutscene inicial do jogo
     jogoInicio();
 
     // Main game loop
@@ -478,6 +650,9 @@ int main(int argc, char** argv)
                 runJogo();
                 telaJogo();
         }
+        #if DEV_MODO
+        dev_print();
+        #endif
         _ED
         //----------------------------------------------------------------------------------
     }
@@ -490,7 +665,7 @@ int main(int argc, char** argv)
     
     CloseWindow();        // Close window and OpenGL context
     
-    #ifdef O_RDWR
+    #if RODANDO_NA_PLACA
     close(fd);
     #endif
     //--------------------------------------------------------------------------------------
